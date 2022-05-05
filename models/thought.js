61ec9682f0f5b5305d2c14175cabc7a001c40e4a
Isaak Morales <ice.isaak97@gmail.com>
2022-05-05 19:01:31 -0400

    getting the models setup

--- models/thought.js
+++ models/thought.js
@@ -13,33 +13,33 @@ reactions: Number,
   lastAccessed: { type: Date, default: Date.now },
 });
 
-// Using mongoose.model() to compile a model based on the schema 'bookSchema'
-const Book = mongoose.model('Book', bookSchema);
+// // Using mongoose.model() to compile a model based on the schema 'bookSchema'
+// const Book = mongoose.model('Book', bookSchema);
 
-const handleError = (err) => console.error(err);
+// const handleError = (err) => console.error(err);
 
-// Create a new instance of the model, a document
-Book.create(
-  {
-    title: 'Diary of Anne Frank',
-    author: 'Anne Frank',
-    publisher: 'Scholastic',
-    stockCount: 10,
-    price: 10,
-    inStock: true,
-  },
-  (err) => (err ? handleError(err) : console.log('Created new document'))
-);
+// // Create a new instance of the model, a document
+// Book.create(
+//   {
+//     title: 'Diary of Anne Frank',
+//     author: 'Anne Frank',
+//     publisher: 'Scholastic',
+//     stockCount: 10,
+//     price: 10,
+//     inStock: true,
+//   },
+//   (err) => (err ? handleError(err) : console.log('Created new document'))
+// );
 
-// Create a new instance with required title and optional author properties
-Book.create(
-  { title: 'Oh the Places You Will Go!', author: 'Dr. Seuss' },
-  (err) => (err ? handleError(err) : console.log('Created new document'))
-);
+// // Create a new instance with required title and optional author properties
+// Book.create(
+//   { title: 'Oh the Places You Will Go!', author: 'Dr. Seuss' },
+//   (err) => (err ? handleError(err) : console.log('Created new document'))
+// );
 
-// Create a new instance with only required title
-Book.create({ title: 'Harold and the Purple Crayon' }, (err) =>
-  err ? handleError(err) : console.log('Created new document')
-);
+// // Create a new instance with only required title
+// Book.create({ title: 'Harold and the Purple Crayon' }, (err) =>
+//   err ? handleError(err) : console.log('Created new document')
+// );
 
 module.exports = thoughtSchema;
